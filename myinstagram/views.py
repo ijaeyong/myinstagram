a05498de147bd45705c837d9caf137927383e0e8
jaeyo <jaeyongish@gmail.com>
2022-02-24 23:48:59 +0900

    모가문제야

--- myinstagram/views.py
+++ myinstagram/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-
+from .views import APIView
 
 class Sub(APIView):
     def get(self, request):
         print("get으로")
-        return render(request, "templates/myinstagram/main.html")
+        return render(request, "myinstagram/main.html")
 
     def post(self, request):
         print("post로")
